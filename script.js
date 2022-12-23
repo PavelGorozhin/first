74aa397b153d2b631da29f05996958d451074859
[Pavel Gorozhin] <[pavel.gorozhin@gmail.com]>
2022-12-23 17:21:42 +1000

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+//var was = 7;
+//var spent = 5;
+//alert(Math.PI);
+//alert(Math.min(5, 9, 12, 22, 91))
+
+var number = 15
+var hasHouse = true
+
+if (number == 5 && hasHouse == false) {
+    alert("Hi");
+} else if (number == 10 || hasHouse) {
+    alert("May be");
+} else {
+    alert("Wrong")
+}
